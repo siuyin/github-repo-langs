774cd11e0b52ef42c1772d81b6db84d73635e251
Loh Siu Yin <siuyin@beyondbroadcast.com>
2020-12-15 09:57:04 +0000

    Refactored main.

--- cmd/scanlangs/main.go
+++ cmd/scanlangs/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/google/go-github/v33/github"
 	"github.com/siuyin/dflt"
 	"golang.org/x/oauth2"
 )
@@ -12,6 +13,14 @@ import (
 func main() {
 	fmt.Println("Scanning Github repository languages")
 
+	client := getClient()
+
+	repos := getRepos(client)
+
+	genCSVReport(repos)
+}
+
+func getClient() *github.Client {
 	tok := dflt.EnvString("TOKEN", "1234....")
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
@@ -20,11 +29,23 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
+	return client
+}
 
-	// list all repositories for the authenticated user
-	repos, _, err := client.Repositories.List(ctx, "", nil)
+func getRepos(client *github.Client) []*github.Repository {
+	repos, _, err := client.Repositories.ListByOrg(context.Background(), "xendit", nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("could not get repos: %v", err)
 	}
-	fmt.Println(repos)
+	return repos
+}
+
+func genCSVReport(repos []*github.Repository) {
+	for _, v := range repos {
+		lang := ""
+		if v.Language != nil {
+			lang = *v.Language
+		}
+		fmt.Println(*v.Name, *v.ID, lang)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/siuyin/github-repo-langs
 
 go 1.15
+
+require (
+	github.com/google/go-github/v33 v33.0.0
+	github.com/siuyin/dflt v0.0.0-20190616123008-ea16caf9b8ef
+	golang.org/x/oauth2 v0.0.0-20201208152858-08078c50e5b5
+)
